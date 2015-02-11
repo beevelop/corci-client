c016ec59986aabe5661220da0f6df06977593db2
Maik Hummel <yo@beevelop.com>
2015-02-11 13:02:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+[![Build Status](https://travis-ci.org/beevelop/corci-client.svg?branch=master)](https://travis-ci.org/beevelop/corci-client)
+[![Dependency Status](https://gemnasium.com/beevelop/corci-client.svg)](https://gemnasium.com/beevelop/corci-client)
+[![Code Climate](https://codeclimate.com/github/beevelop/corci-client/badges/gpa.svg)](https://codeclimate.com/github/beevelop/corci-client)
+[![GitHub release](https://img.shields.io/github/release/beevelop/corci-client.svg?style=flat)](https://github.com/beevelop/corci-client/releases)
+[![GitHub issues](https://img.shields.io/github/issues/beevelop/corci-client.svg?style=flat)](https://github.com/beevelop/corci-client/issues)
+
+# CorCI-client
+
+> A convenient way to handle your own cordova builds without relying on Phonegap Build.
+
+# Disclaimer
+
+This project is currently under heavy development and might be unstable. Don't use it in production (unless you're adventurous).
+
+# Usage
+
+@todo
---
+++ bin/cmd.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+var path = require('path');
+var Client = require('../lib/Client');
+
+var Common = require('corci-libs').Common;
+var yargs = Common.yargs;
+var fs = Common.fsExtra;
+
+var patch = require('corci-libs').patch;
+// patch on to support binding with multiple events at once
+patch(process.EventEmitter.prototype, ["on", "addListener"]);
+
+var conf = yargs
+    .help('help')
+    .version('0.0.1', 'v')
+    .alias('v', 'version')
+    .showHelpOnFail(true)
+    .usage('Sends a build request to the corCI-master.\nUsage: $0\nAdditionally you can append platform-specific files by using the target name (e.g. android) as an argument.')
+    .options('p', {
+        alias: 'port',
+        default: 8000,
+        describe: 'Port the client should connect to'
+    })
+    .options('q', {
+        alias: 'protocol',
+        default: 'http',
+        describe: 'Protocol the server is reachable at (https requires key and cert argument)'
+    })
+    .options('h', {
+        alias: 'host',
+        default: 'localhost',
+        describe: 'the server\'s hostname'
+    })
+    .options('k', {
+        alias: 'keep',
+        default: 0,
+        describe: 'Amount of builds in location to keep (0 = unlimited)'
+    })
+    .options('l', {
+        alias: 'location',
+        describe: 'Path to directory where binaries should be stored (leave empty to not store binaries)'
+    })
+    .options('t', {
+        alias: 'target',
+        default: 'autodetect',
+        describe: 'Define a target (e.g. android); use multiple times for multiple targets'
+    })
+    .options('f', {
+        alias: 'file',
+        describe: 'Path to the cordova project\'s zip file',
+        require: true
+    })
+    .options('n', {
+        alias: 'name',
+        default: 'corci',
+        describe: 'the app\'s name'
+    })
+    .check(function (args, opts) {
+        if (!args.name.match(/^[a-z0-9-]+$/)) {
+            throw 'Error: name should follow these rules: [a-z0-9-]'
+        }
+
+        var filePath = path.resolve(args.file);
+        if (fs.existsSync(filePath)) {
+            args.file = filePath;
+        } else {
+            throw 'Error: file could not be found (note: the path has to be relative to your current working directory)';
+        }
+    })
+    .config('build.json') // should be optional
+    .argv;
+
+// Convert conf.target to an array if it is a string
+// additionally split by comma to cover usage mistakes
+if (typeof conf.target === 'string') {
+    conf.target = conf.target.split(',');
+}
+
+// Assemble the master's socket url from the provided conf values
+conf.url = '{0}://{1}{2}/{3}'.format(conf.protocol, conf.host, conf.port === 80 ? '' : ':' + conf.port, 'client');
+
+if (!conf.location) {
+    conf.save = false;
+}
+
+var client = new Client(conf);
+client.connect();
